2ea445c0952cb4330ebc741c7ccbfc7724bf9ed6
CherryYui <76659804+CherryYu-beep@users.noreply.github.com>
2025-05-22 03:55:46 +0300

    удаление лишнего

--- TaikoQT/main.cpp
+++ TaikoQT/main.cpp
@@ -6,29 +6,7 @@
 #include <QJsonObject>
 #include <QJsonArray>
 
-QVariantList loadChart()
-{
-    QFile file(":/notes.json");
-    if (!file.open(QIODevice::ReadOnly)) {
-        qWarning("Couldn't open chart.json");
-        return {};
-    }
 
-    QByteArray data = file.readAll();
-    QJsonDocument doc = QJsonDocument::fromJson(data);
-
-    QVariantList result;
-
-    if (doc.isObject()) {
-        QJsonObject root = doc.object();
-        QJsonArray notes = root["notes"].toArray();
-        for (const auto &val : notes) {
-            result.append(val.toObject().toVariantMap());
-        }
-    }
-
-    return result;
-}
 
 int main(int argc, char *argv[])
 {
@@ -36,8 +14,8 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
-    QVariantList chartData = loadChart();
-    engine.rootContext()->setContextProperty("chartData", chartData);
+
+
 
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
